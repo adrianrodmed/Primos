dcf9048eee38741e3a01636eae060f9a2eb6e670
adrianrodmed <34574699+adrianrodmed@users.noreply.github.com>
2017-12-21 21:42:34 +0100

    Add files via upload

--- numerosprimos/WebContent/primos.js
+++ numerosprimos/WebContent/primos.js
@@ -1,24 +1,15 @@
-var txt = document.getElementById("numero").value;
+var cnt = 0;
 function primos(){
-	var primo;
-	primo=true;
-	for(var i=2; i<=txt-1; i++){
-		if(txt%i==0){//txt%i significa el resto de la división
-			primo=false;
-			break;}}
-	return primo; //devuelve el resultado de la funcion verdadera
+	var txt = parseInt(document.getElementById("numero").value);
+	var primo = true;
+	for(var i = 2; i <= Math.round(txt/2) && primo == true; i++){
+		if(txt%i == 0){//txt%i significa el resto de la división
+			primo=false;}
+	if(primo){
+		console.log("El número " + txt + " es primo.");
+		cnt++;}
+	else{
+		window.alert("El número " + txt + " no es primo, tu puntuación es: "+ cnt + ".");
+		cnt = 0;}
+	}
 }
-if(primos(txt)){
-	window.alert("El número es primo");}
-else{
-	window.alert("El número no es primo");}
-
-//var cnt=0;
-//function puntuacion(){
-//	if(text!==primo){
-//		alert('El juego ha terminado');
-//		cnt=0;}
-//	else{
-//		alert('!BIEN HECHO! Has acertado');
-//		cnt++;}
-//	}
