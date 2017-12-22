f0f5add9d838223406e45b557d00563387185a74
adrianrodmed <34574699+adrianrodmed@users.noreply.github.com>
2017-12-22 10:54:42 +0100

    Add files via upload

--- numerosprimos/WebContent/primos.js
+++ numerosprimos/WebContent/primos.js
@@ -4,12 +4,13 @@ function primos(){
 	var primo = true;
 	for(var i = 2; i <= Math.round(txt/2) && primo == true; i++){
 		if(txt%i == 0){//txt%i significa el resto de la división
-			primo=false;}
+			primo=false;
+			break;}
+	}
 	if(primo){
 		console.log("El número " + txt + " es primo.");
 		cnt++;}
 	else{
 		window.alert("El número " + txt + " no es primo, tu puntuación es: "+ cnt + ".");
 		cnt = 0;}
-	}
 }
